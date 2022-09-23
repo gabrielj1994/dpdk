176bf1f266c24a9b3082925a0f6b5edfda0a8b4c
gabrielj1994 <gabrielj1994@gmail.com>
2022-09-22 22:04:17 -0400

    Revert "Squash: Clean up"
    This reverts commit 2c4a8ed855f44d4fe61c5c8bc6ed671da0200c32.

--- examples/skeleton/basicfwd.c
+++ examples/skeleton/basicfwd.c
@@ -139,11 +139,101 @@ lcore_main(struct rte_mempool *mbuf_pool)
 					"not be optimal.\n", port);
 	}
 
-	printf("\nCore %u sending ICMP Echo request packets. [Ctrl+C to quit]\n",
+	printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n",
 			rte_lcore_id());
 
 	// MIT 6.5810 LAB1: hard-coded values for ICMP echo request
 	const uint16_t request_size = 1;
+	// char echo_request[] = {0x00, 0xab, 0x78, 0x00,
+	// 				0x01, 0x00, 0x00, 0x00,
+	// 				0x00, 0xab, 0xf8, 0x7c,
+	// 				0x0f, 0x00, 0x00, 0x00,
+	// 				0x80, 0x00, 0x01, 0x00,
+	// 				0x01, 0x00, 0x01, 0x00,
+	// 				0x02, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x91, 0x06, 0x00, 0x00,
+	// 				0x62, 0x00, 0x00, 0x00,
+	// 				0x62, 0x00, 0x00, 0x00,
+	// 				0xe2, 0x08, 0xe9, 0xea,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x80, 0x08,
+	// 				0xc0, 0xe4, 0x21, 0x00,
+	// 				0x01, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x00, 0x00,
+	// 				0x14, 0x58, 0xd0, 0x58,
+	// 				0x9f, 0x93, 0x14, 0x58,
+	// 				0xd0, 0x58, 0x5f, 0xb3,
+	// 				0x08, 0x00, 0x45, 0x00,
+	// 				0x00, 0x54, 0x3f, 0xab,
+	// 				0x40, 0x00, 0x40, 0x01,
+	// 				0x77, 0xa8, 0xc0, 0xa8,
+	// 				0x01, 0x02, 0xc0, 0xa8,
+	// 				0x01, 0x03, 0x08, 0x00,
+	// 				0x0f, 0xb6, 0x00, 0x18,
+	// 				0x00, 0x01, 0x80, 0xdf,
+	// 				0x2c, 0x63, 0x00, 0x00,
+	// 				0x00, 0x00, 0x71, 0x1b,
+	// 				0x0b, 0x00, 0x00, 0x00,
+	// 				0x00, 0x00, 0x10, 0x11,
+	// 				0x12, 0x13, 0x14, 0x15,
+	// 				0x16, 0x17, 0x18, 0x19,
+	// 				0x1a, 0x1b, 0x1c, 0x1d,
+	// 				0x1e, 0x1f, 0x20, 0x21,
+	// 				0x22, 0x23, 0x24, 0x25,
+	// 				0x26, 0x27, 0x28, 0x29,
+	// 				0x2a, 0x2b, 0x2c, 0x2d,
+	// 				0x2e, 0x2f, 0x30, 0x31,
+	// 				0x32, 0x33, 0x34, 0x35,
+	// 				0x36, 0x37};
+
 	char echo_request[] = {0x14, 0x58, 0xd0, 0x58,
 					0x9f, 0x93, 0x14, 0x58,
 					0xd0, 0x58, 0x5f, 0xb3,
@@ -182,7 +272,9 @@ lcore_main(struct rte_mempool *mbuf_pool)
 			// MIT 6.5810 LAB 1: Only use port1
 			if (port != 1) continue;
 
+			/* Get burst of RX packets, from first port of pair. */
 			struct rte_mbuf *bufs[BURST_SIZE];
+			// bufs[0] = malloc(sizeof(echo_request)/sizeof(echo_request[0]));
 			bufs[0] = rte_pktmbuf_alloc(mbuf_pool);
 			char *data;
 			data = rte_pktmbuf_mtod(bufs[0], char*);
@@ -193,6 +285,96 @@ lcore_main(struct rte_mempool *mbuf_pool)
 			mbuf->data_len = sizeof(echo_request)/sizeof(echo_request[0]);
 			mbuf->pkt_len = sizeof(echo_request)/sizeof(echo_request[0]);
 
+			// Capture a whole packet
+			char *prtp = (char *)bufs[0];
+			
+			uint16_t pkt_len = rte_pktmbuf_pkt_len(bufs[0]);
+			printf("\nLOGGING: MBuf Log [pkt_len=%u, data=%p, bufs_0=%p]\n", pkt_len, data, bufs[0]);
+
+			//Sanity check print from bufs[0] to data
+			printf("\nLOGGING: BUFS[0] to data\n");
+			uint16_t counter = 0;
+			while (prtp != data) {
+				printf("%02hhx ", *prtp);
+				++counter;
+				if (counter % 4 == 0)
+					printf("\n");
+				++prtp;
+				if (counter >= sizeof(echo_request)/sizeof(echo_request[0])) {
+					printf("\nLOGGING: Failsafe triggered\n");
+					break;
+				}
+			}
+
+			printf("\nLOGGING: Counter log [counter=%u]\n", counter);
+
+			counter = 0;
+			for(prtp = data; prtp < data + pkt_len; ++prtp) {
+				// printf("\nLOGGING: Data Log [position=%u, char_val=%hhx]\n", counter, *prtp);
+				printf("%02hhx", *prtp);
+				++counter;
+				if (counter % 4 == 0)
+					printf("\n");
+				//LAB1: Failsafe
+				if (counter >= sizeof(echo_request)/sizeof(echo_request[0])) {
+					printf("\nLOGGING: Failsafe triggered\n");
+					break;
+				}
+			}
+			// continue;
+
+
+			// //print whole packet
+
+			// // uint16_t pkt_len;
+			// // struct rte_mbuf *mbuf;
+			
+			// printf("\nLOGGING: Dummy log [test_0=%u]\n", (uint16_t)sizeof(test[0]));
+			// struct rte_ether_hdr *ether_hdr;
+			// struct rte_ipv4_hdr *ipv4_hdr;
+			// struct rte_icmp_hdr *icmp_hdr;
+			// struct rte_ether_addr ether_src;
+			// uint32_t ip_addr_src;
+			// uint16_t cksum;
+
+			// uint16_t pkt_counter = 0;
+			// while (pkt_counter < nb_rx) {
+			// 	//
+			// 	// pkt_len = rte_pktmbuf_pkt_len(bufs[pkt_counter]);
+			// 	//todo: is this needed?
+			// 	// mbuf = rte_pktmbuf_mtod(bufs[pkt_counter], struct rte_mbuf *);
+
+			// 	// The function documentation is wrong. Cast type is 2nd parameter, additional offset is 3rd parameter
+			// 	ether_hdr = rte_pktmbuf_mtod_offset(bufs[pkt_counter], struct rte_ether_hdr *, 0);
+			// 	ipv4_hdr = rte_pktmbuf_mtod_offset(bufs[pkt_counter], struct rte_ipv4_hdr *, sizeof(struct rte_ether_hdr));
+			// 	icmp_hdr = rte_pktmbuf_mtod_offset(bufs[pkt_counter], struct rte_icmp_hdr *, sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr));
+
+			// 	//ether frame
+			// 	rte_ether_addr_copy(&ether_hdr->src_addr, &ether_src);
+			// 	rte_ether_addr_copy(&ether_hdr->dst_addr, &ether_hdr->src_addr);
+			// 	rte_ether_addr_copy(&ether_src, &ether_hdr->dst_addr);
+
+			// 	//ipv4
+			// 	ip_addr_src = ipv4_hdr->src_addr;
+			// 	ipv4_hdr->src_addr = ipv4_hdr->dst_addr;
+			// 	ipv4_hdr->dst_addr = ip_addr_src;
+
+			// 	//icmp
+			// 	icmp_hdr->icmp_cksum = 0;
+			// 	icmp_hdr->icmp_type = RTE_IP_ICMP_ECHO_REPLY;
+			// 	//TODO: calculate icmp packet size (data len - headers)
+			// 	cksum = rte_raw_cksum(icmp_hdr, 64);
+			// 	icmp_hdr->icmp_cksum = (uint16_t)~cksum;
+
+			// 	// strftime(filename, sizeof(filename), "/opt/log/reply_packet_dump_%Y%m%d_%H%M%S", timenow);
+
+			// 	// fp = fopen(filename, "w");
+			// 	// rte_pktmbuf_dump(fp, bufs[pkt_counter], pkt_len);
+			// 	// printf("\nLOGGING: Packets dumped to file [filename=%s]\n", filename);
+			// 	++pkt_counter;
+			// }
+			// fclose(fp);
+			
 			/* Send ICMP echo request through TX packets. */
 			const uint16_t nb_tx = rte_eth_tx_burst(port, 0,
 					bufs, request_size);
@@ -209,7 +391,6 @@ lcore_main(struct rte_mempool *mbuf_pool)
 					rte_pktmbuf_free(bufs[buf]);
 			} else {
 				while (microseconds < 10000000) {
-					// 10 second time out
 					const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
 					bufs, BURST_SIZE);
 					elapsed_cycles = rte_rdtsc_precise() - begin; 
@@ -220,7 +401,9 @@ lcore_main(struct rte_mempool *mbuf_pool)
 				}
 
 				if (microseconds < 10000000) {
+					//TODO: output metrics
 					printf("\nLOGGING: ICMP Echo Statistics [packets_transmitted=%u, packets_received=%u, time=%lu microseconds]\n", request_size, request_size, microseconds);
+
 				} else {
 					printf("\nLOGGING: ICMP Echo request timeout after 10 seconds\n");
 				}
